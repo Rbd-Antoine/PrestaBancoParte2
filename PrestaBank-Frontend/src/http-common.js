0ebc16683e5f94e8d012e0f58a15aa391102405f
Rbd-Antoine <122039637+Rbd-Antoine@users.noreply.github.com>
2024-12-03 12:17:28 -0300

    Parte2 new

---
+++ PrestaBank-Frontend/src/http-common.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const Evaluacion1BackendServer = "127.0.0.1";
+const Evaluacion1BackendPort = "63221";
+
+console.log(Evaluacion1BackendServer)
+console.log(Evaluacion1BackendPort)
+
+export default axios.create({
+    baseURL: `http://${Evaluacion1BackendServer}:${Evaluacion1BackendPort}`,
+    headers: {
+        'Content-Type': 'application/json'
+    }
+});
